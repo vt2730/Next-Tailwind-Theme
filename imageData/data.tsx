65f2185c7591c5477240884204547a2314ff3783
vikash <vikash.tiwari@underscoretec.com>
2023-04-09 11:13:29 +0530

    theme added to ui

---
+++ imageData/data.tsx
@@ -0,0 +1,30 @@
+import a from "../Images/a.jpeg"
+import b from "../Images/b.jpeg"
+import c from "../Images/c.jpeg"
+import one from "../Images/one.jpg"
+import two from "../Images/two.jpg"
+import three from "../Images/one.jpg"
+
+export const ImageData = [
+    {
+        img: one,
+        tag: "Article",
+        title: "Boost your conversion rate",
+        footer: "Roel Aufderehar",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum."
+    },
+    {
+        img: two,
+        tag: "Video",
+        title: "How to use search engine optimization to drive sales",
+        footer: "Brenna Goyette",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum."
+    },
+    {
+        img: three,
+        tag: "Case Study",
+        title: "Improve your customer experience",
+        footer: "Daniela Metz",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum."
+    }
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import a from '../Images/a.jpeg'
-
+import {ImageData} from '../imageData/data'
 
 const themes = [
-  { name: "Light" },
-  { name: "Dark" },
-  { name: "Gruvbox" },
-  { name: "Pink" },
-];
+  { name: 'Light' },
+  { name: 'Dark', },
+  { name: 'Emerald', },
+  { name: 'Pink', },
+]
 
 const ToggleButton = () => {
   const [mounted, setMounted] = useState(false);
@@ -51,7 +51,59 @@ export default function Home() {
       <ToggleButton />
 
       <main>
-     
+        <div className="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 bg-th-background">
+          <div className="relative max-w-7xl mx-auto">
+            <div className="text-center">
+              <h2 className="text-3xl tracking-tight font-extrabold text-th-accent-medium sm:text-4xl">
+                SuperCars Blog
+              </h2>
+              <p className="mt-3 max-w-2xl mx-auto text-xl sm:mt-4">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.
+              </p>
+            </div>
+
+            <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+              {
+                ImageData?.map((item: any, idx: any) => {
+                  return (
+                    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden" key={idx}>
+                      <div className="flex-shrink-0">
+                        <Image src={item?.img} width={420} height={50} alt="" style={{ objectFit: "cover" }} />
+                      </div>
+                      <div className="flex-1 bg-th-background-secondary p-6 flex flex-col justify-between">
+                        <div className="flex-1">
+                          <p className="text-sm font-medium text-th-accent-medium">
+                            <a href="#" className="hover:underline">
+                              {item?.tag}
+                            </a>
+                          </p>
+                          <a href="#" className="block mt-2">
+                            <p className="text-xl font-semibold">
+                              {item?.title}
+                            </p>
+                            <p className="mt-3 text-base">
+                              {item?.description}
+                            </p>
+                          </a>
+                        </div>
+                        <div className="mt-6 flex items-center">
+                          <div className="ml-3">
+                            <p className="text-sm font-medium">
+                              <a href="#" className="hover:underline">
+                                {item?.footer}
+                              </a>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+
+          </div>
+        </div>
       </main>
     </div>
   )
